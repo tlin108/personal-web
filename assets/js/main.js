290aa78e57dfb5237da70ff17840155ac380b872
tlin108 <tlin108@hotmail.com>
2017-01-16 21:47:27 -0500

    add nav bar and data-anchor for quick scroll to sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,37 @@ jQuery(document).ready(function($) {
         });
 
     });
-   
-    
 
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 100) {
+            $('nav').addClass('fixed');
+            $('.main-wrapper section').each(function(i) {
+                if ($(this).position().top <= windscroll) {
+                    $('nav li.active').removeClass('active');
+                    $('nav li').eq(i).addClass('active');
+                }
+            });
+
+        } else {
+
+            $('nav').removeClass('fixed');
+            $('nav a.active').removeClass('active');
+            $('nav a:first').addClass('active');
+        }
+
+    }).scroll()
+
+    $('nav a').on('click', function() {
+
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 70;
+
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
+
+        return false;
+
+    })
 });
